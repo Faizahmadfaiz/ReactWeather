79b4b4d76afdd716a0aedcab5d65be9236e91fcf
FAIZ <faizahmadf@gmail.com>
2017-02-26 12:35:39 +0530

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-const path = require('path');
+var path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
@@ -8,6 +17,6 @@ app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
-app.listen(3000, function() {
-  console.log('Server is running on PORT 3000');
+app.listen(PORT, function() {
+  console.log('Server is running on PORT ', PORT);
 });
