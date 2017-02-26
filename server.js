c547a63a23c04cab54f5b9a223f8b5d47b85c4f1
FAIZ <faizahmadf@gmail.com>
2017-02-26 13:09:43 +0530

    Messed up

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 var path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   if(req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
-});
+});*/
 
 app.use(express.static('public'));
 
@@ -17,6 +17,6 @@ app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
-app.listen(PORT, function() {
-  console.log('Server is running on PORT ', PORT);
+app.listen(3000, function() {
+  console.log('Server is running on PORT 3000');
 });
