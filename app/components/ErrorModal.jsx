0cf3c7288ae5ca3b916e59cdf1b29341c870bbda
FAIZ <faizahmadf@gmail.com>
2017-02-28 13:01:07 +0530

    Fix error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     getDefaultProps: function() {
@@ -11,14 +13,8 @@ var ErrorModal = React.createClass({
         message: React.PropTypes.string.isRequired
     },
     componentDidMount: function() {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    },
-    render: function() {
-        console.log("Error Modal");
         var {title, message} = this.props;
-
-        return(
+        var modalMarkup = (
              <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}</p>
@@ -29,7 +25,22 @@ var ErrorModal = React.createClass({
                 </p>
             </div>
         );
+
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render: function() {
+        return (
+            <div></div>
+        );
     }
 });
 
 module.exports = ErrorModal;
+
+//<div id="error-modal" class="reveal tiny text-center" data-reveal="w3ayql-reveal" data-reactid=".0.1.0.0.3" role="dialog" aria-hidden="true" data-yeti-box="error-modal" data-resize="error-modal" tabindex="-1" style="display: none; top: 0px;"><h4 data-reactid=".0.1.0.0.3.0">Error</h4><p data-reactid=".0.1.0.0.3.1">city not found</p><p data-reactid=".0.1.0.0.3.2"><button class="button hollow" data-close="" data-reactid=".0.1.0.0.3.2.0">Okay</button></p></div>
+
+//<div id="error-modal" class="reveal tiny text-center" data-reveal="2v5e0y-reveal" data-reactid=".0.1.0.0.3" role="dialog" aria-hidden="false" data-yeti-box="error-modal" data-resize="error-modal" tabindex="-1" style="display: block; top: 0px;"><h4 data-reactid=".0.1.0.0.3.0">Error</h4><p data-reactid=".0.1.0.0.3.1">city not found</p><p data-reactid=".0.1.0.0.3.2"><button class="button hollow" data-close="" data-reactid=".0.1.0.0.3.2.0">Okay</button></p></div>
